10ea1aa403b93384239f75f8cd78ea9ba2c80d9c
Sharan Yalburgi <sharanyalburgi@gmail.com>
2018-05-31 13:39:47 +0530

    Remove treedict dependency

--- pymc4/model.py
+++ pymc4/model.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
-import seaborn as sns
 import threading
-import matplotlib.pyplot as plt
 
 __all__ = ["Model"]
 
@@ -20,47 +18,6 @@ class Context(object):
         type(self).get_contexts().pop()
 
 
-def withparent(meth):
-    """Helper wrapper that passes calls to parent's instance"""
-    def wrapped(self, *args, **kwargs):
-        res = meth(self, *args, **kwargs)
-        if getattr(self, 'parent', None) is not None:
-            getattr(self.parent, meth.__name__)(*args, **kwargs)
-        return res
-    # Unfortunately functools wrapper fails
-    # when decorating built-in methods so we
-    # need to fix that improper behaviour
-    wrapped.__name__ = meth.__name__
-    return wrapped
-
-
-class treedict(dict):
-    """A dict that passes mutable extending operations used in Model
-    to parent dict instance.
-    Extending treedict you will also extend its parent
-    """
-    def __init__(self, iterable=(), parent=None, **kwargs):
-        super(treedict, self).__init__(iterable, **kwargs)
-        assert isinstance(parent, dict) or parent is None
-        self.parent = parent
-        if self.parent is not None:
-            self.parent.update(self)
-    # typechecking here works bad
-    __setitem__ = withparent(dict.__setitem__)
-    update = withparent(dict.update)
-
-    def tree_contains(self, item):
-        # needed for `add_random_variable` method
-        if isinstance(self.parent, treedict):
-            return (dict.__contains__(self, item) or
-                    self.parent.tree_contains(item))
-        elif isinstance(self.parent, dict):
-            return (dict.__contains__(self, item) or
-                    self.parent.__contains__(item))
-        else:
-            return dict.__contains__(self, item)
-
-
 class Model(Context):
     def __new__(cls, *args, **kwargs):
         instance = super(Model, cls).__new__(cls)
@@ -74,10 +31,7 @@ class Model(Context):
 
     def __init__(self, name="", model=None, ):
         self.name = name
-        if self.parent is not None:
-            self.named_vars = treedict(parent=self.parent.named_vars)
-        else:
-            self.named_vars = treedict()
+        self.named_vars = {}
 
     @property
     def model(self):
@@ -105,7 +59,7 @@ class Model(Context):
 
     def add_random_variable(self, var):
         """Add a random variable to the named variables of the model."""
-        if self.named_vars.tree_contains(var.name):
+        if var.name in self.named_vars:
             raise ValueError(
                 "Variable name {} already exists.".format(var.name))
         self.named_vars[var.name] = var

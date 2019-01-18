85e3b210d96c189e4011d841228b98684334f590
Ravin Kumar <ravinsdrive@gmail.com>
2019-01-18 08:20:52 -0800

    Remove duplicated normal test

--- pymc4/tests/test_random_variables.py
+++ pymc4/tests/test_random_variables.py
@@ -52,19 +52,6 @@ def tf_supported_args():
     return _tfp_supported_args
 
 
-def test_normal_dist(tf_session):
-    """Small test of RandomVariable functionality.
-
-    Test is intended to be deprecated and removed as full testing functionality is built out
-    """
-
-    normal_dist = _random_variables.Normal("test_normal", loc=0, scale=1)
-    log_prob = normal_dist.log_prob()
-
-    vals = tf_session.run([log_prob], feed_dict={normal_dist._backend_tensor: 0})
-    assert np.isclose(vals[0], -0.918_938_5)
-
-
 def test_tf_session_cleared(tf_session):
     """Temporary test: Check that fixture is finalizing correctly"""
     assert len(tf_session.graph.get_operations()) == 0

a76ebfc54582e082cdd888e4ce003da8378419c7
Eric Ma <ericmajinglong@gmail.com>
2019-02-06 14:07:51 -0500

    Added docstrings to Normal distribution and developer notes

--- pymc4/random_variables/continuous.py
+++ pymc4/random_variables/continuous.py
@@ -44,7 +44,74 @@ class LogitNormal(RandomVariable):
 
 class Normal(RandomVariable):
     """
-    Normal distribution.
+    Univariate normal distribution.
+
+    The pdf of this distribution is
+
+    .. math::
+
+       f(x \mid \mu, \tau) =
+           \sqrt{\frac{\tau}{2\pi}}
+           \exp\left\{ -\frac{\tau}{2} (x-\mu)^2 \right\}
+
+    Normal distribution can be parameterized either in terms of precision
+    or standard deviation. The link between the two parametrizations is
+    given by
+
+    .. math::
+
+       \tau = \dfrac{1}{\sigma^2}
+
+    .. plot::
+
+        import matplotlib.pyplot as plt
+        import numpy as np
+        import scipy.stats as st
+        plt.style.use('seaborn-darkgrid')
+        x = np.linspace(-5, 5, 1000)
+        mus = [0., 0., 0., -2.]
+        sigmas = [0.4, 1., 2., 0.4]
+        for mu, sigma in zip(mus, sigmas):
+            pdf = st.norm.pdf(x, mu, sigma)
+            plt.plot(x, pdf, label=r'$\mu$ = {}, $\sigma$ = {}'.format(mu, sigma))
+        plt.xlabel('x', fontsize=12)
+        plt.ylabel('f(x)', fontsize=12)
+        plt.legend(loc=1)
+        plt.show()
+
+    ========  ==========================================
+    Support   :math:`x \in \mathbb{R}`
+    Mean      :math:`\mu`
+    Variance  :math:`\dfrac{1}{\tau}` or :math:`\sigma^2`
+    ========  ==========================================
+
+    Parameters
+    ----------
+    mu : float
+        Mean.
+    sigma : float
+        Standard deviation (sigma > 0) (only required if tau is not specified).
+
+    Examples
+    --------
+    .. code-block:: python
+        with pm.Model():
+            x = pm.Normal('x', mu=0, sigma=10)
+        with pm.Model():
+            x = pm.Normal('x', mu=0, tau=1/23)
+
+    Developer Notes
+    ---------------
+
+    Parameter mappings to TensorFlow Probability are as follows:
+
+    - mu: loc
+    - sigma: scale
+    """
+
+    def _base_dist(self, mu, sigma, *args, **kwargs):
+        return tfd.Normal(loc=mu, scale=sigma, **kwargs)
+
 
     This class is re-implemented so as to provide a familiar API to PyMC3 users.
     Here, we intentionally break TFD's style guide to make it easier to port

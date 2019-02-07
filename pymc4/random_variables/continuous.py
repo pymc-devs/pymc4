c7f225e0734d556a53e7774330be42a940bff222
Eric Ma <ericmajinglong@gmail.com>
2019-02-07 13:31:41 -0500

    updated developer notes for vonmises dist

--- pymc4/random_variables/continuous.py
+++ pymc4/random_variables/continuous.py
@@ -583,6 +583,49 @@ class InverseGamma(RandomVariable):
         return tfd.InverseGamma(concentration=alpha, rate=beta, *args, **kwargs)
 
 
+class Kumaraswamy(RandomVariable):
+    R"""
+    Kumaraswamy random variable.
+
+    The pdf of this distribution is
+
+    .. math::
+
+       f(x \mid a, b) =
+           abx^{a-1}(1-x^a)^{b-1}
+
+    .. plot::
+
+        import matplotlib.pyplot as plt
+        import numpy as np
+        plt.style.use('seaborn-darkgrid')
+        x = np.linspace(0, 1, 200)
+        a_s = [.5, 5., 1., 2., 2.]
+        b_s = [.5, 1., 3., 2., 5.]
+        for a, b in zip(a_s, b_s):
+            pdf = a * b * x ** (a - 1) * (1 - x ** a) ** (b - 1)
+            plt.plot(x, pdf, label=r'$a$ = {}, $b$ = {}'.format(a, b))
+        plt.xlabel('x', fontsize=12)
+        plt.ylabel('f(x)', fontsize=12)
+        plt.ylim(0, 3.)
+        plt.legend(loc=9)
+        plt.show()
+
+    ========  ==============================================================
+    Support   :math:`x \in (0, 1)`
+    Mean      :math:`b B(1 + \tfrac{1}{a}, b)`
+    Variance  :math:`b B(1 + \tfrac{2}{a}, b) - (b B(1 + \tfrac{1}{a}, b))^2`
+    ========  ==============================================================
+
+    Parameters
+    ----------
+    a : float
+        a > 0.
+    b : float
+        b > 0.
+    """
+
+
 class LogitNormal(RandomVariable):
     def _base_dist(self, *args, **kwargs):
         """
@@ -841,6 +884,13 @@ class VonMises(RandomVariable):
         Mean.
     kappa : float
         Concentration (\frac{1}{kappa} is analogous to \sigma^2).
+
+    Developer Notes
+    ---------------
+    Parameter mappings to TensorFlow Probability are as follows:
+
+    - mu: loc
+    - kappa: concentration
     """
     def _base_dist(self, mu, kappa, *args, **kwargs):
         return tfd.VonMises(loc=mu, concentration=kappa, *args, **kwargs)

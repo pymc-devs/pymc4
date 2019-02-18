55ed5654dd04ed445edb4410980359e72fec487d
Eric Ma <ericmajinglong@gmail.com>
2019-02-17 22:23:57 -0500

    initial implementation of transforms

---
+++ pymc4/random_variables/transforms.py
@@ -0,0 +1,70 @@
+from tensorflow_probability import bijectors
+
+class Transform:
+    """
+    A transformation of a random variable from one space into another.
+
+    Attributes
+    ----------
+    name : str
+    """
+    def __init__(self, name="", bijector=None):
+        self.name = name
+        self.bijector = bijector
+
+    def forward(self, x):
+        """
+        Applies transformation forward to input variable `x`.
+
+        When transform is used on some distribution `p`, it will transform the random variable `x` after sampling
+        from `p`.
+
+        Parameters
+        ----------
+        x : tensor
+            Input tensor to be transformed.
+
+        Returns
+        --------
+        tensor
+            Transformed tensor.
+        """
+        return self.bijector.forward(x)
+
+    def backward(self, x):
+        """
+        Applies inverse of transformation to input variable `z`.
+
+        When transform is used on some distribution `p`, which has observed values `z`, it is used to
+        transform the values of `z` correctly to the support of `p`.
+
+        Parameters
+        ----------
+        z : tensor
+            Input tensor to be inverse transformed.
+
+        Returns
+        -------
+        tensor
+            Inverse transformed tensor.
+        """
+        return self.bijector.inverse(x)
+
+    def logjacdet(self, x):
+        """
+        Calculates logarithm of the absolute value of the Jacobian determinant for input `x`.
+
+        Parameters
+        ----------
+        x : tensor
+            Input to calculate Jacobian determinant of.
+
+        Returns
+        -------
+        tensor
+            The log abs Jacobian determinant of `x` w.r.t. this transform.
+        """
+        return self.bijector.forward_log_det_jacobian(x, x.shape.ndims)
+
+
+logodds = Transform(name="logodds", bijector=bijectors.Invert(bijectors.Sigmoid))

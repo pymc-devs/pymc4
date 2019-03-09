f56c393b58752f22c9520a20fc5c2bbf6d364aea
Thomas Wiecki <thomas.wiecki@gmail.com>
2019-03-09 14:09:34 +0100

    Add test for auto naming.

---
+++ pymc4/tests/test_autoname.py
@@ -0,0 +1,29 @@
+"""
+Tests for PyMC4 auto naming.
+"""
+
+import pytest
+from .. import random_variables, _template_contexts, model
+
+import pymc4 as pm
+from pymc4 import Normal
+
+def create_rvs(**kwargs):
+    return pm.Normal(0, 1, name='test')
+
+@pm.model(auto_name=True)
+def autoname_test():
+    mu = pm.Normal(0, 1)
+    mu2 = Normal(0, 1)
+    mu3 = pm.random_variables.Normal(0, 1)
+    pm.Normal(0, 1, name='custom')
+    dummy = pm.Normal(0, 1, name='custom2')
+    x = create_rvs()
+
+def test_auto_name():
+    model = autoname_test.configure()
+
+    rv_names = [rv.name for rv in model._forward_context.vars]
+    expected_rv_names = ['mu', 'mu2', 'mu3', 'custom', 'custom2', 'test']
+
+    assert rv_names == expected_rv_names

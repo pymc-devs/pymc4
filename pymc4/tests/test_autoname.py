a220cea6c85b7a4c7f36d4b4cfe8373e52bde7a4
Thomas Wiecki <thomas.wiecki@gmail.com>
2019-03-10 14:51:54 +0100

    Make test clearer.

--- pymc4/tests/test_autoname.py
+++ pymc4/tests/test_autoname.py
@@ -2,21 +2,26 @@
 Tests for PyMC4 auto naming.
 """
 
+# AST parsing only works in non-nested functions, thus place things into module namespace
 import pymc4 as pm
+
+# AST looks different if we use pm.Normal or just Normal
 from pymc4 import Normal
 
 
 def create_rvs(**kwargs):
-    return pm.Normal(0, 1, name="test")
+    # AST parsers should just add name kwargs to pymc4 RVs
+    assert "name" not in kwargs.keys()
+    return pm.Normal(0, 1, name="inside_function")
 
 
 @pm.model(auto_name=True)
 def autoname_test():
-    mu = pm.Normal(0, 1)
-    mu2 = Normal(0, 1)
-    mu3 = pm.random_variables.Normal(0, 1)
-    pm.Normal(0, 1, name="custom")
-    dummy = pm.Normal(0, 1, name="custom2")
+    inferred_w_module = pm.Normal(0, 1)
+    inferred_wo_module = Normal(0, 1)
+    inferred_full_path = pm.random_variables.Normal(0, 1)
+    pm.Normal(0, 1, name="supplied_name")
+    dummy = pm.Normal(0, 1, name="overwrite_name")
     x = create_rvs()
 
 
@@ -24,6 +29,13 @@ def test_auto_name():
     model = autoname_test.configure()
 
     rv_names = [rv.name for rv in model._forward_context.vars]
-    expected_rv_names = ["mu", "mu2", "mu3", "custom", "custom2", "test"]
+    expected_rv_names = [
+        "inferred_w_module",
+        "inferred_wo_module",
+        "inferred_full_path",
+        "supplied_name",
+        "overwrite_name",
+        "inside_function",
+    ]
 
     assert rv_names == expected_rv_names

241392b56e3425075427e201c416864eee839f1c
Ravin Kumar <ravinsdrive@gmail.com>
2019-04-09 21:24:59 -0700

    Fix Wishart and VonMisesFisher tests

--- pymc4/tests/test_random_variables.py
+++ pymc4/tests/test_random_variables.py
@@ -49,10 +49,10 @@ def random_variable_args():
         (random_variables.Triangular, {"lower": 0.0, "upper": 1.0, "c": 0.5}),
         (random_variables.Uniform, {"lower": 0, "upper": 1}),
         (random_variables.VonMises, {"mu": 0, "kappa": 1}),
-        (random_variables.VonMisesFisher, {"mu": [0, 1], "kappa": 1, "sample": [0, 1]}),
+        (random_variables.VonMisesFisher, {"mu": [0, 1], "kappa": 1, "sample": [0.0, 1.0]}),
         (random_variables.Weibull, {"beta": 0.1, "alpha": 1.0}),
-        (random_variables.Wishart, {"nu": 3, "V": [[1]], "sample": [[1]]}),
-        (random_variables.ZeroInflatedBinomial, {"psi": 0.2, "n": 10, "p": 0.5, "sample": 0}),
+        (random_variables.Wishart, {"nu": 3, "V": [[1]], "sample": [[1.0]]}),
+        (random_variables.ZeroInflatedBinomial, {"psi": 0.2, "n": 10, "p": 0.5, "sample": 0.0}),
         (
             random_variables.ZeroInflatedNegativeBinomial,
             {"psi": 0.2, "mu": 10, "alpha": 3, "sample": 0},

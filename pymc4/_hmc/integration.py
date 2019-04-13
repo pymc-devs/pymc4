66589d3a3860a12db379642201b5df81b5a89c5d
Thomas Wiecki <thomas.wiecki@gmail.com>
2019-04-13 11:34:41 +0200

    Port HMC from pymc3 (#98)
    * Initial port of PyMC3 HMC and NUTS sampler. At least seems to run.
    
    * Update NBs to work with tf2 and new tfp.
    
    * Add step_size debug info. Use pre-allocated array.
    
    * Update leapfrog integrator
    
    * Apply black
    
    * Actually use make lint

--- pymc4/_hmc/integration.py
+++ pymc4/_hmc/integration.py
@@ -1,6 +1,4 @@
 from collections import namedtuple
-
-import numpy as np
 from scipy import linalg
 
 
@@ -16,17 +14,9 @@ class CpuLeapfrogIntegrator(object):
         """Leapfrog integrator using CPU."""
         self._potential = potential
         self._logp_dlogp_func = logp_dlogp_func
-        self._dtype = self._logp_dlogp_func.dtype
-        if self._potential.dtype != self._dtype:
-            raise ValueError(
-                "dtypes of potential (%s) and logp function (%s)"
-                "don't match." % (self._potential.dtype, self._dtype)
-            )
 
     def compute_state(self, q, p):
         """Compute Hamiltonian functions using a position and momentum."""
-        if q.dtype != self._dtype or p.dtype != self._dtype:
-            raise ValueError("Invalid dtype. Must be %s" % self._dtype)
         logp, dlogp = self._logp_dlogp_func(q)
         v = self._potential.velocity(p)
         kinetic = self._potential.energy(p, velocity=v)
@@ -52,7 +42,7 @@ class CpuLeapfrogIntegrator(object):
         None if `out` is provided, else a State namedtuple
         """
         try:
-            return self._step(epsilon, state, out=None)
+            return self._step(epsilon, state)
         except linalg.LinAlgError as err:
             msg = "LinAlgError during leapfrog step."
             raise IntegrationError(msg)
@@ -65,42 +55,24 @@ class CpuLeapfrogIntegrator(object):
             else:
                 raise
 
-    def _step(self, epsilon, state, out=None):
+    def _step(self, epsilon, state):
         pot = self._potential
-        axpy = linalg.blas.get_blas_funcs("axpy", dtype=self._dtype)
-
         q, p, v, q_grad, energy, logp = state
-        if out is None:
-            q_new = q.copy()
-            p_new = p.copy()
-            v_new = np.empty_like(q)
-            q_new_grad = np.empty_like(q)
-        else:
-            q_new, p_new, v_new, q_new_grad, energy = out
-            q_new[:] = q
-            p_new[:] = p
 
         dt = 0.5 * epsilon
 
-        # p is already stored in p_new
-        # p_new = p + dt * q_grad
-        axpy(q_grad, p_new, a=dt)
+        # half momentum step
+        p_new = p + dt * q_grad
 
-        pot.velocity(p_new, out=v_new)
-        # q is already stored in q_new
-        # q_new = q + epsilon * v_new
-        axpy(v_new, q_new, a=epsilon)
+        # whole position step
+        v_new = pot.velocity(p_new)
+        q_new = (q + epsilon * v_new).astype(q.dtype)
 
-        logp = self._logp_dlogp_func(q_new, q_new_grad)
-
-        # p_new = p_new + dt * q_new_grad
-        axpy(q_new_grad, p_new, a=dt)
+        # half momentum step
+        logp, q_new_grad = self._logp_dlogp_func(q_new)
+        p_new = p_new + dt * q_new_grad
 
         kinetic = pot.velocity_energy(p_new, v_new)
         energy = kinetic - logp
 
-        if out is not None:
-            out.energy = energy
-            return
-        else:
-            return State(q_new, p_new, v_new, q_new_grad, energy, logp)
+        return State(q_new, p_new, v_new, q_new_grad, energy, logp)

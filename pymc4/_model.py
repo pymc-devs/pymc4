d7d1a469500733969887522581c3f9e465ea1043
Thomas Wiecki <thomas.wiecki@gmail.com>
2019-05-29 15:00:35 -0400

    Add single and multi-chain sampling. Improve examples.

--- pymc4/_model.py
+++ pymc4/_model.py
@@ -82,10 +82,12 @@ class Model:
         self._template._func(*args, **kwargs)
         return
 
-    def make_log_prob_function(self):
+    def make_log_prob_function(self, logp_sum_func=None):
         """Return the log probability of the model."""
+        if logp_sum_func is None:
+            logp_sum_func = self.logp_sum_single
 
-        def log_prob(*args, **kwargs):
+        def log_prob(*args, logp_sum_func=logp_sum_func, **kwargs):
             varnames = [v.name for v in self._forward_context.vars]
             if args:
                 kwargs.update({k:v for k,v in zip(varnames, args)})
@@ -101,11 +103,18 @@ class Model:
             context = contexts.InferenceContext([kwargs[v.name] for v in self._forward_context.vars], expected_vars=self._forward_context.vars)
             with context:
                 self._evaluate()
-                logp = sum(tf.reduce_sum(var.log_prob(),
-                                         axis=tf.range(1, tf.rank(var.log_prob()))) for var in context.vars)
+                logp = logp_sum_func(context)
                 return logp
 
         return log_prob
+
+    def logp_sum_single(self, context):
+        return sum(tf.reduce_sum(var.log_prob()) for var in context.vars)
+
+    def logp_sum_multi(self, context):
+        return sum(tf.reduce_sum(
+            var.log_prob(),
+            axis=tf.range(1, tf.rank(var.log_prob()))) for var in context.vars)
 
     def forward_sample(self, *args, **kwargs):
         """Simulate data from the model via forward sampling."""
@@ -119,34 +128,53 @@ class Model:
         model._observations.update(kwargs)
         return model
 
-    def sample_posterior(self, num_results=100, num_burnin_steps=10, num_leapfrog_steps=10, step_size=0.005, num_chains=10):
+    def sample_posterior(self, samples=1000, tuning=500, leapfrog_steps=10, step_size=0.005, num_chains=1, compile=True):
         """Samples from the posterior of model."""
-        log_prob_func = tf.function(self.make_log_prob_function())
+        samples += tuning
+
+        if compile:
+            function = tf.function
+        else:
+            function = lambda x: x
+
+        if num_chains == 1:
+            forward_sample = self.forward_sample()
+            logp_sum_func = self.logp_sum_single
+        else:
+            forward_sample = tf.vectorized_map(self.forward_sample, tf.range(num_chains))
+            logp_sum_func = self.logp_sum_multi
+
+        log_prob_func = function(self.make_log_prob_function(logp_sum_func=logp_sum_func))
         # Create input tensors
-        var_names = [var.name for var in self._forward_context.vars]
-        forward_sample = tf.vectorized_map(self.forward_sample, tf.range(num_chains))
+        var_names = [var.name for var in self._forward_context.vars if var.name not in self._observations.keys()]
+
 
         initial_state = [
             forward_sample[var_name] for var_name in var_names
         ]
 
-        kernel = tfp.mcmc.HamiltonianMonteCarlo(
-            target_log_prob_fn=log_prob_func,
-            num_leapfrog_steps=num_leapfrog_steps,
-            step_size=step_size)
+        @function
+        def hmc_tfp(log_prob_func, initial_state, samples, tuning, leapfrog_steps, step_size):
+            kernel = tfp.mcmc.HamiltonianMonteCarlo(
+                target_log_prob_fn=log_prob_func,
+                num_leapfrog_steps=leapfrog_steps,
+                step_size=step_size)
 
-        kernel = tfp.mcmc.SimpleStepSizeAdaptation(
-            inner_kernel=kernel, num_adaptation_steps=int(num_burnin_steps * 0.8))
+            kernel = tfp.mcmc.SimpleStepSizeAdaptation(
+                inner_kernel=kernel, num_adaptation_steps=int(tuning * 0.8))
 
-        samples, stats = tfp.mcmc.sample_chain(
-            num_results=num_results,
-            num_burnin_steps=num_burnin_steps,
-            current_state=initial_state,
-            kernel=kernel,
-            #trace_fn=None,
-        )
+            trace, stats = tfp.mcmc.sample_chain(
+                num_results=samples,
+                num_burnin_steps=tuning,
+                current_state=initial_state,
+                kernel=kernel,
+            )
 
+            return trace, stats
+
+        trace, stats = hmc_tfp(log_prob_func, initial_state, samples, tuning, leapfrog_steps, step_size)
         # Create arviz trace
-        trace = {var_name: np.swapaxes(sample.numpy()[num_burnin_steps:], 0, 1) for var_name, sample in zip(var_names, samples)}
-        trace = az.from_dict(trace)
-        return trace, stats, forward_sample
+        # trace_az = {var_name: np.swapaxes(t.numpy()[tuning:], 0, 1) for var_name, t in zip(var_names, trace)}
+        trace_az = {var_name: t.numpy()[tuning:][np.newaxis, ...] for var_name, t in zip(var_names, trace)}
+        trace_az = az.from_dict(trace_az)
+        return trace_az, stats

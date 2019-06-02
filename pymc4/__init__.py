43040f9153524f5dbf154947422be5f990c8a858
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-06-02 15:03:38 +0300

    better error handling

--- pymc4/__init__.py
+++ pymc4/__init__.py
@@ -1,9 +1,9 @@
 """PyMC4."""
 
+from .scopes import name_scope
 from . import coroutine_model
 from . import distributions
 from . import flow
 from .coroutine_model import Model, model
-from pymc4.scopes import name_scope
 
 __version__ = "0.0.1"

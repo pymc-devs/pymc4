f733239dbb9406833c3f7de0e15abb960ec56fd6
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-07-03 00:39:52 +0300

    test logp simple

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -1,3 +1,4 @@
+import pytest
 import pymc4 as pm
 import numpy as np
 from pymc4 import distributions as dist
@@ -86,3 +87,33 @@ def test_transformed_model_transformed_executor_with_passed_value():
 
     assert set(state.values) == {"n", "__log_n"}
     np.testing.assert_allclose(state.values["n"], 1.0)
+
+
+def test_transformed_executor_logp_tensorflow():
+    pytest.importorskip("tensorflow_probability")
+    from tensorflow_probability import bijectors as bij, distributions as tfd
+    norm_log = tfd.TransformedDistribution(tfd.HalfNormal(1), bij.Invert(bij.Exp()))
+
+    def transformed_model():
+        norm = yield dist.HalfNormal("n", 1, transform=dist.transforms.Log())
+        return norm
+
+    _, state = pm.evaluate_model_transformed(transformed_model(), __log_n=-np.pi)
+    np.testing.assert_allclose(state.collect_log_prob(), norm_log.log_prob(-np.pi), equal_nan=False)
+
+    _, state = pm.evaluate_model_transformed(transformed_model(), n=np.exp(-np.pi))
+    np.testing.assert_allclose(state.collect_log_prob(), norm_log.log_prob(-np.pi), equal_nan=False)
+
+
+def test_executor_logp_tensorflow():
+    pytest.importorskip("tensorflow_probability")
+    from tensorflow_probability import distributions as tfd
+    norm = tfd.HalfNormal(1)
+
+    def transformed_model():
+        norm = yield dist.HalfNormal("n", 1, transform=dist.transforms.Log())
+        return norm
+
+    _, state = pm.evaluate_model(transformed_model(), n=np.pi)
+
+    np.testing.assert_allclose(state.collect_log_prob(), norm.log_prob(np.pi), equal_nan=False)

91d388501aa14eb23e430706e99df5d4d42c5483
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-07-02 17:59:33 +0300

    remove biwrap as a dependency

--- pymc4/utils.py
+++ pymc4/utils.py
@@ -1,3 +1,6 @@
+import functools
+
+
 def map_nested(fn, structure, cond=lambda obj: True):
     r"""
     Applies fn to an object in a possibly nested data structure and returns same
@@ -46,3 +49,20 @@ def merge_dicts(*dicts: dict, **kwargs: dict):
             )
         kwargs.update(mappable)
     return kwargs
+
+
+def biwrap(wrapper):
+    @functools.wraps(wrapper)
+    def enhanced(*args, **kwargs):
+        is_bound_method = hasattr(args[0], wrapper.__name__) if args else False
+        if is_bound_method:
+            count = 1
+        else:
+            count = 0
+        if len(args) > count:
+            newfn = wrapper(*args, **kwargs)
+            return newfn
+        else:
+            newwrapper = functools.partial(wrapper, *args, **kwargs)
+            return newwrapper
+    return enhanced

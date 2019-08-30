fd58c524257013b25f5afa8fdae7b79591706062
Thomas Wiecki <thomas.wiecki@gmail.com>
2019-08-30 13:16:44 +0200

    Add more continuous distributions

--- pymc4/distributions/tensorflow/__init__.py
+++ pymc4/distributions/tensorflow/__init__.py
@@ -1,4 +1,3 @@
-from pymc4.distributions.tensorflow.continuous import Normal
-from pymc4.distributions.tensorflow.continuous import HalfNormal
+from pymc4.distributions.tensorflow.continuous import *
 from pymc4.distributions.tensorflow.distribution import Potential
 from pymc4.distributions.tensorflow import transforms
--- pymc4/distributions/tensorflow/continuous.py
+++ pymc4/distributions/tensorflow/continuous.py
@@ -47,3 +47,121 @@ class HalfNormal(BackendDistribution, abstract.HalfNormal):
     def _init_backend(self):
         sigma = self.conditions["sigma"]
         self._backend_distribution = tfd.HalfNormal(scale=sigma)
+
+
+class Beta(BackendDistribution, abstract.Beta):
+    def _init_backend(self):
+        alpha, beta = self.conditions["alpha"], self.conditions["beta"]
+        self._backend_distribution = tfd.Beta(concentration0=alpha, concentration1=beta)
+
+
+class Cauchy(BackendDistribution, abstract.Cauchy):
+    def _init_backend(self, alpha, beta):
+        alpha, beta = self.conditions["alpha"], self.conditions["beta"]
+        self.backend_distribution = tfd.Cauchy(loc=alpha, scale=beta)
+
+
+class ChiSquared(Back, abstract.ChiSquared):
+    def _init_backend(self):
+        nu = self.conditions["nu"]
+        self.backend_distribution = tfd.Chi2(df=nu)
+
+
+class Exponential(BackendDistribution, Exponential):
+    def _init_backend(self):
+        lam = self.conditions["lam"]
+        self.backend_distribution = tfd.Exponential(rate=lam)
+
+
+class Gamma(BackendDistribution, Gamma):
+    def _init_backend(self):
+        alpha, beta = self.conditions["alpha"], self.conditions["beta"]
+        self.backend_distribution = tfd.Gamma(concentration=alpha, rate=beta)
+
+
+class Gumbel(BackendDistribution, Gumbel):
+    def _init_backend(self):
+        alpha, beta = self.conditions["alpha"], self.conditions["beta"]
+        self.backend_distribution = tfd.Gumbel(loc=mu, scale=beta)
+
+
+class HalfCauchy(BackendDistribution, HalfCauchy):
+    def _init_backend(self):
+        beta = self.conditions["beta"]
+        self.backend_distribution = tfd.HalfCauchy(loc=0, scale=beta)
+
+
+class InverseGamma(BackendDistribution, abstract.InverseGamma):
+    def _init_backend(self):
+        alpha, beta = self.conditions["alpha"], self.conditions["beta"]
+        self.backend_distribution = tfd.InverseGamma(concentration=alpha, rate=beta)
+
+
+class InverseGaussian(BackendDistribution, abstract.InverseGaussian):
+    def _init_backend(self):
+        mu, lam = self.conditions["mu"], self.conditions["lam"]
+        self.backend_distribution = tfd.InverseGaussian(loc=mu, concentration=lam)
+
+
+class Kumaraswamy(BackendDistribution, abstract.Kumaraswamy):
+    def _init_backend(self):
+        a, b = self.conditions["a"], self.conditions["b"]
+        self.backend_distribution = tfd.Kumaraswamy(concentration0=a, concentration1=b)
+
+
+class Laplace(BackendDistribution, abstract.Laplace):
+    def _init_backend(self):
+        mu, b = self.conditions["mu"], self.conditions["b"]
+        self.backend_distribution = tfd.Laplace(loc=mu, scale=b)
+
+
+class Logistic(BackendDistribution, abstract.Logistic):
+    def _init_backend(self):
+        mu, s = self.conditions["mu"], self.conditions["s"]
+        self.backend_distribution = tfd.Logistic(loc=mu, scale=s)
+
+
+class LogitNormal(BackendDistribution, abstract.LogitNormal):
+    def _init_backend(self):
+        mu, sigma = self.conditions["mu"], self.conditions["sigma"]
+        self.backend_distribution = tfd.TransformedDistribution(
+            distribution=tfd.Normal(loc=mu, scale=sigma, *args, **kwargs),
+            bijector=tfp.bijectors.Sigmoid(),
+            name="LogitNormal",
+        )
+
+
+class LogNormal(BackendDistribution, abstract.LogNormal):
+    def _init_backend(self):
+        mu, sigma = self.conditions["mu"], self.conditions["sigma"]
+        self.backend_distribution = tfd.LogNormal(loc=mu, scale=sigma)
+
+
+class Pareto(BackendDistribution, abstract.Pareto):
+    def _init_backend(self):
+        alpha, m = self.conditions["alpha"], self.conditions["m"]
+        self.backend_distribution = tfd.Pareto(concentration=alpha, scale=m)
+
+
+class StudentT(BackendDistribution, abstract.StudentT):
+    def _init_backend(self):
+        nu, mu, sigma = self.conditions["nu"], self.conditions["mu"], self.conditions["sigma"]
+        self.backend_distribution = tfd.StudentT(df=nu, loc=mu, scale=sigma)
+
+
+class Triangular(BackendDistribution, abstract.Triangular):
+    def _init_backend(self):
+        lower, upper, c = self.conditions["lower"], self.conditions["upper"], self.conditions["c"]
+        self.backend_distribution = tfd.Triangular(low=lower, high=upper, peak=c)
+
+
+class Uniform(BackendDistribution, abstract.Uniform):
+    def _init_backend(self):
+        lower, upper = self.conditions["lower"], self.conditions["upper"]
+        self.backend_distribution = tfd.Uniform(low=lower, high=upper)
+
+
+class VonMises(BackendDistribution, abstract.VonMises):
+    def _init_backend(self):
+        mu, kappa = self.conditions["mu"], self.conditions["kappa"]
+        self.backend_distribution = tfd.VonMises(loc=mu, concentration=kappa)

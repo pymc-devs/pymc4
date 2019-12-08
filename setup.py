66e452f31f1e3e5c01fd6bab6b4ba21224d92419
George Ho <19851673+eigenfoo@users.noreply.github.com>
2019-12-08 17:06:32 -0500

    Remove Python 3.5 classifier from setup.py (#183)

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ LICENSE = "Apache License, Version 2.0"
 CLASSIFIERS = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "License :: OSI Approved :: Apache Software License",

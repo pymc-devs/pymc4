6ea88cec674bad23b0ce86df58e82825350a22ac
Eric Ma <ericmajinglong@gmail.com>
2019-12-30 21:17:37 -0500

    First commit of notebook auto-build

---
+++ nbconfig.py
@@ -0,0 +1,8 @@
+c = get_config()
+c.NbConvertApp.notebooks = [
+    "notebooks/baseball.ipynb",
+    "notebooks/basic-usage.ipynb",
+    "notebooks/rugby_analytics.ipynb",
+    "notebooks/radon_hierarchical.ipynb",
+]
+

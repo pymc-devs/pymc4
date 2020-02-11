890a33604b5dec3ec710f42672abbdfec2765b16
Sayam753 <sayamkumar753@yahoo.in>
2020-02-11 22:12:02 +0530

    Add initial_state and observed_time_series parameter

--- pymc4/distributions/timeseries.py
+++ pymc4/distributions/timeseries.py
@@ -12,10 +12,16 @@ class AR(ContinuousDistribution):
             Total number of timesteps to model.
     coefficients : float Tensor
             Autoregressive coefficients of shape `concat(batch_shape, [order])`.
+            order = coefficients.shape[-1] (order>0)
     level_scale : Scalar float Tensor
             Standard deviation of the transition noise at each step
             (any additional dimensions are treated as batch
             dimensions).
+    initial_state : (Optional) float Tensor
+            Corresponding values of size `order` for
+            imagined timesteps before the initial step.
+    observed_time_series : (Optional) float Tensor
+            Observed time series
     initial_step : (Optional) int Tensor
             Starting timestep (Default value: 0).
 
@@ -27,12 +33,28 @@ class AR(ContinuousDistribution):
             x = pm.AR('x', num_timesteps=50, coefficients=[0.2, -0.8], level_scale=-0.2)
     """
 
-    def __init__(self, name, num_timesteps, coefficients, level_scale, initial_step=0, **kwargs):
+    def __init__(
+        self,
+        name,
+        num_timesteps,
+        coefficients,
+        level_scale,
+        initial_state=None,
+        observed_time_series=None,
+        initial_step=0,
+        **kwargs,
+    ):
+        if initial_state is not None and observed_time_series is None:
+            raise TypeError(
+                "Missing parameter 'observed_time_series' if 'initial_state' is specified"
+            )
         super().__init__(
             name,
             num_timesteps=num_timesteps,
             coefficients=coefficients,
             level_scale=level_scale,
+            initial_state=initial_state,
+            observed_time_series=observed_time_series,
             initial_step=initial_step,
             **kwargs,
         )
@@ -42,10 +64,21 @@ class AR(ContinuousDistribution):
         num_timesteps = conditions["num_timesteps"]
         coefficients = conditions["coefficients"]
         level_scale = conditions["level_scale"]
+        initial_state = conditions["initial_state"]
+        observed_time_series = conditions["observed_time_series"]
         initial_step = conditions["initial_step"]
+
         coefficients = tf.convert_to_tensor(value=coefficients, name="coefficients")
         order = tf.compat.dimension_value(coefficients.shape[-1])
-        time_series_object = sts.Autoregressive(order=order)
+
+        if initial_step is not None and observed_time_series is not None:
+            initial_state = tf.convert_to_tensor(value=initial_state)
+            observed_time_series = tf.convert_to_tensor(value=observed_time_series)
+            observed_time_series = tf.concat([initial_state, observed_time_series], axis=0)
+
+        time_series_object = sts.Autoregressive(
+            order=order, observed_time_series=observed_time_series
+        )
         distribution = time_series_object.make_state_space_model(
             num_timesteps=num_timesteps,
             param_vals={"coefficients": coefficients, "level_scale": level_scale},

a3d9a46c246edd5ac82343efd3be765b6dc5ba82
lucianopaz <luciano.paz.neuro@gmail.com>
2020-02-17 08:48:00 +0100

    Added meta executor

--- pymc4/__init__.py
+++ pymc4/__init__.py
@@ -5,7 +5,12 @@ from .scopes import name_scope, variable_name
 from . import coroutine_model
 from . import distributions
 from . import flow
-from .flow import evaluate_model_transformed, evaluate_model, evaluate_model_posterior_predictive
+from .flow import (
+    evaluate_model_transformed,
+    evaluate_model,
+    evaluate_model_posterior_predictive,
+    evaluate_meta_model,
+)
 from .coroutine_model import Model, model
 from . import inference
 from .distributions import *
--- pymc4/flow/__init__.py
+++ pymc4/flow/__init__.py
@@ -2,16 +2,20 @@
 from .executor import SamplingExecutor, SamplingState
 from .transformed_executor import TransformedSamplingExecutor
 from .posterior_predictive_executor import PosteriorPredictiveSamplingExecutor
+from .meta_executor import MetaSamplingExecutor
 
 __all__ = [
     "SamplingExecutor",
     "TransformedSamplingExecutor",
     "PosteriorPredictiveSamplingExecutor",
+    "MetaSamplingExecutor",
     "evaluate_model",
     "evaluate_model_transformed",
     "evaluate_model_posterior_predictive",
+    "evaluate_meta_model",
 ]
 
 evaluate_model = SamplingExecutor()
 evaluate_model_transformed = TransformedSamplingExecutor()
 evaluate_model_posterior_predictive = PosteriorPredictiveSamplingExecutor()
+evaluate_meta_model = MetaSamplingExecutor()

045f9800b3a92b620e9920da1966c5e048f459c8
Simon Dirmeier <simon.dirmeier@web.de>
2020-06-15 23:16:08 +0200

    Add prototype mixture model

--- pymc4/distributions/mixture.py
+++ pymc4/distributions/mixture.py
@@ -1,7 +1,15 @@
+"""PyMC4 Distribution of a random variable consisting of a mixture of other
+distributions.
+
+Wraps tfd.Mixture as pm.Mixture
+"""
+import tensorflow as tf
+from tensorflow_probability import distributions as tfd
+
+import pymc4 as pm
 from pymc4.distributions.distribution import Distribution
 
 
-# TODO: Implement this
 class Mixture(Distribution):
     r"""
     Mixture random variable.
@@ -19,7 +27,7 @@ class Mixture(Distribution):
     ----------
     p : array of floats
         p >= 0 and p <= 1
-        the mixture weights, in the form of probabilities
+        the mixture weights, in the form of probabilities, must sum to one.
     distributions : multidimensional PyMC4 distribution (e.g. `pm.Poisson(...)`)
         or iterable of one-dimensional PyMC4 distributions the
         component distributions :math:`f_1, \ldots, f_n`
@@ -27,3 +35,16 @@ class Mixture(Distribution):
 
     def __init__(self, name, p, distributions, **kwargs):
         super().__init__(name, p=p, distributions=distributions, **kwargs)
+
+    @staticmethod
+    def _init_distribution(conditions):
+        p, distributions = conditions["p"], conditions["distributions"]
+        if isinstance(p, pm.Categorical):
+            cat = p._distribution
+        else:
+            cat = tfd.Categorical(probs=p)
+        if isinstance(distributions, list):
+            distributions = [d._distribution for d in distributions]
+        elif isinstance(distributions, Distribution):
+            return
+        return tfd.Mixture(cat=cat, components=distributions)

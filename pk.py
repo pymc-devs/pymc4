90c1a12fb47d87913954b66fe133ab867009a8c1
Rasul Karimov <yoatom96@hotmail.com>
2020-06-20 21:17:13 +0300

    remove unn files

--- pk.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-
-def f(d):
-    d = d ** 2
-    return d
--- untitled0.py
+++
@@ -1,8 +0,0 @@
-import pymc4 as pm
-import numpy as np
-import arviz as az
-
-# define parametrization
-J = 8
-y = np.array([28, 8, -3, 7, -1, 1, 18, 12], dtype=np.float32)
-sigma = np.array([15, 10, 16, 11, 9, 11, 10, 18], dtype=np.float32)

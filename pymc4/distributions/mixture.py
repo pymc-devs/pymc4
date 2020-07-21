df4a8734fdbb70c849fbffa8af1bc5f49a4b0c93
Simon Dirmeier <dirmeier@users.noreply.github.com>
2020-07-21 21:28:36 +0200

    Implement mixture distribution (#284)
    * Add prototype mixture model
    
    * Allow batched PyMC4 distributions
    
    * Black code
    
    * Simplify mixture initialization and add test cases
    
    * Make Pylint happy
    
    * Minor fixes at mixture
    
    * Add some tests and documentation
    
    * Minor fixes
    
    * Minor fixes
    
    * Add XLA fixture to use in sampling test
    
    * Replace XLA fixture with conftest-fixture
    
    * Set use_static_graph for Mixture
    
    * Catch failure in xla test
    
    * Fix import of tf.errors

--- pymc4/distributions/mixture.py
+++ pymc4/distributions/mixture.py
@@ -1,7 +1,17 @@
+"""PyMC4 Distribution of a random variable consisting of a mixture of other
+distributions.
+
+Wraps tfd.Mixture as pm.Mixture
+"""
+
+import collections
+from typing import Union, Tuple, List
+
+import tensorflow as tf
+from tensorflow_probability import distributions as tfd
 from pymc4.distributions.distribution import Distribution
 
 
-# TODO: Implement this
 class Mixture(Distribution):
     r"""
     Mixture random variable.
@@ -17,13 +27,85 @@ class Mixture(Distribution):
 
     Parameters
     ----------
-    p : array of floats
+    p : tf.Tensor
         p >= 0 and p <= 1
-        the mixture weights, in the form of probabilities
-    distributions : multidimensional PyMC4 distribution (e.g. `pm.Poisson(...)`)
-        or iterable of one-dimensional PyMC4 distributions the
-        component distributions :math:`f_1, \ldots, f_n`
+        The mixture weights, in the form of probabilities,
+        must sum to one on the last (i.e., right-most) axis.
+    distributions : pm.Distribution|sequence of pm.Distribution
+        Multi-dimensional PyMC4 distribution (e.g. `pm.Poisson(...)`)
+        or iterable of one-dimensional PyMC4 distributions
+        :math:`f_1, \ldots, f_n`
+
+    Examples
+    --------
+    Let's define a simple two-component Gaussian mixture:
+
+    >>> import tensorflow as tf
+    >>> import pymc4 as pm
+    >>> @pm.model
+    ... def mixture(dat):
+    ...     p = tf.constant([0.5, 0.5])
+    ...     m = yield pm.Normal("means", loc=tf.constant([0.0, 0.0]), scale=1.0)
+    ...     comps = pm.Normal("comps", m, scale=1.0)
+    ...     obs = yield pm.Mixture("mix", p=p, distributions=comps, observed=dat)
+    ...     return obs
+
+    The above implementation only allows components of the same family of distribitions.
+    In order to allow for different families, we need a more verbose implementation:
+
+    >>> @pm.model
+    ... def mixture(dat):
+    ...     p = tf.constant([0.5, 0.5])
+    ...     m = yield pm.Normal("means", loc=tf.constant([0.0, 0.0]), scale=1.0)
+    ...     comp1 = pm.Normal("comp1", m[..., 0], scale=1.0)
+    ...     comp2 = pm.StudentT("comp2", m[..., 1], scale=1.0, df=3)
+    ...     obs = yield pm.Mixture("mix", p=p, distributions=[comp1, comp2], observed=dat)
+    ...     return obs
+
+    We can also, as usual with Tensorflow, use higher dimensional parameters:
+
+    >>> @pm.model
+    ... def mixture(dat):
+    ...     p = tf.constant([[0.8, 0.2], [0.4, 0.6], [0.5, 0.5]])
+    ...     m = yield pm.Normal("means", loc=tf.constant([[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]]), scale=1.0)
+    ...     comp1 = pm.Normal("d1", m[..., 0], scale=1.0)
+    ...     comp2 = pm.StudentT("d2", m[..., 1], scale=1.0, df=3)
+    ...     obs = yield pm.Mixture("mix", p=p, distributions=[comp1, comp2], observed=dat)
+    ...     return obs
+
+    Note that in the last implementation the mixing weights need to sum to one
+    on the right-most axis (to ensure correct parameterization use `validate_args=True`)
     """
 
-    def __init__(self, name, p, distributions, **kwargs):
+    def __init__(
+        self,
+        name: str,
+        p: tf.Tensor,
+        distributions: Union[Distribution, List[Distribution], Tuple[Distribution]],
+        **kwargs,
+    ):
         super().__init__(name, p=p, distributions=distributions, **kwargs)
+
+    @staticmethod
+    def _init_distribution(conditions, **kwargs):
+        p, d = conditions["p"], conditions["distributions"]
+        # if 'd' is a sequence of pymc distributions, then use the underlying
+        # tfp distributions for the mixture
+        if isinstance(d, collections.abc.Sequence):
+            if any(not isinstance(el, Distribution) for el in d):
+                raise TypeError(
+                    "every element in 'distribution' needs to be a pymc4.Distribution object"
+                )
+            distr = [el._distribution for el in d]
+            return tfd.Mixture(
+                tfd.Categorical(probs=p, **kwargs), distr, **kwargs, use_static_graph=True
+            )
+        # else if 'd' is a pymc distribution with batch_size > 1
+        elif isinstance(d, Distribution):
+            return tfd.MixtureSameFamily(
+                tfd.Categorical(probs=p, **kwargs), d._distribution, **kwargs
+            )
+        else:
+            raise TypeError(
+                "'distribution' needs to be a pymc4.Distribution object or a sequence of distributions"
+            )

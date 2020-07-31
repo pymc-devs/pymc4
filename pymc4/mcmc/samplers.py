4204d99143d66207bfaa59ddea114bc09bc731db
Rasul Karimov <yoatom96@hotmail.com>
2020-07-31 04:54:30 +0300

    add seed

--- pymc4/mcmc/samplers.py
+++ pymc4/mcmc/samplers.py
@@ -61,7 +61,8 @@ class _BaseSampler(metaclass=abc.ABCMeta):
         observed: Optional[dict] = None,
         state: Optional[flow.SamplingState] = None,
         use_auto_batching: bool = True,
-        xla=False,
+        xla: bool = False,
+        seed: int = None,
         is_compound=False,
     ):
         """
@@ -105,10 +106,10 @@ class _BaseSampler(metaclass=abc.ABCMeta):
 
         if xla:
             results, sample_stats = tf.xla.experimental.compile(
-                self._run_chains, inputs=[init_state, burn_in,],
+                self._run_chains, inputs=[init_state, burn_in, seed],
             )
         else:
-            results, sample_stats = self._run_chains(init_state, burn_in,)
+            results, sample_stats = self._run_chains(init_state, burn_in, seed)
 
         posterior = dict(zip(init_keys, results))
         # Keep in sync with pymc3 naming convention
@@ -125,7 +126,7 @@ class _BaseSampler(metaclass=abc.ABCMeta):
         return trace_to_arviz(posterior, sampler_stats, observed_data=state_.observed_values)
 
     @tf.function(autograph=False)
-    def _run_chains(self, init, burn_in):
+    def _run_chains(self, init, burn_in, seed):
         kernel = self._kernel(target_log_prob_fn=self.parallel_logpfn, **self.kernel_kwargs)
         if self._adaptation:
             adapt_kernel = self._adaptation(inner_kernel=kernel, **self.adaptation_kwargs,)
@@ -138,6 +139,7 @@ class _BaseSampler(metaclass=abc.ABCMeta):
             kernel=adapt_kernel,
             num_burnin_steps=burn_in,
             trace_fn=self._trace_fn,
+            seed=seed,
             **self.chain_kwargs,
         )
         return results, sample_stats
--- pymc4/mcmc/tf_support.py
+++ pymc4/mcmc/tf_support.py
@@ -1,5 +1,4 @@
 import functools
-from typing import NamedTuple
 import collections
 import numpy as np
 
@@ -19,7 +18,7 @@ def _target_log_prob_fn_part(*state_part, idx, len_, state, target_log_prob_fn):
     return log_prob
 
 
-def kernel_create_object(sampleri, curr_indx, setli, current_state, target_log_prob_fn):
+def kernel_create_object(sampleri, curr_indx, setli, current_state, target_log_prob_fn, seed):
     mkf = sampleri[0]
     kernel = mkf.kernel(
         target_log_prob_fn=functools.partial(
@@ -30,6 +29,7 @@ def kernel_create_object(sampleri, curr_indx, setli, current_state, target_log_p
             target_log_prob_fn=target_log_prob_fn,
         ),
         **{**sampleri[1], **mkf.kernel_kwargs},
+        seed=seed,
     )
     if mkf.adaptive_kernel:
         kernel = mkf.adaptive_kernel(inner_kernel=kernel, **mkf.adaptive_kwargs)
@@ -37,20 +37,37 @@ def kernel_create_object(sampleri, curr_indx, setli, current_state, target_log_p
 
 
 class _CompoundStepTF(kernel_base.TransitionKernel):
-    """
-        Simple support for compound step
-        TODO:
-    """
+    def __init__(
+        self,
+        target_log_prob_fn,
+        compound_samplers,
+        compound_set_lengths,
+        name=None,
+    ):
+        """
+           Initializes the compound step transition kernel
 
-    def __init__(self, target_log_prob_fn, compound_samplers, compound_set_lengths, name=None):
-        self._target_log_prob_fn = target_log_prob_fn
-        self._compound_samplers = [
-            (sampler[0]._default_kernel_maker(), sampler[1]) for sampler in compound_samplers
-        ]
-        self._compound_set_lengths = compound_set_lengths
-        self._cumulative_lengths = np.cumsum(compound_set_lengths) - compound_set_lengths
-        self._name = name
-        self._parameters = dict(target_log_prob_fn=target_log_prob_fn, name=name,)
+           Args:
+             target_log_prob_fn: Python callable which takes an argument like
+               `current_state` (or `*current_state` if it's a list) and returns its
+               (possibly unnormalized) log-density under the target distribution.
+             compound_samplers: List of the pymc4.mcmc.samplers._BaseSampler sub-classes
+               that are used for each subset of the free variables
+             compound_set_lengths: List of the sizes of each subset of variables
+     >>      seed: Python integer to seed the random number generator. Deprecated, pass
+               seed to `tfp.mcmc.sample_chain`.
+             name: Python `str` name prefixed to Ops created by this function.
+        """
+
+        with tf.name_scope(name or "CompoundSampler") as name:
+            self._target_log_prob_fn = target_log_prob_fn
+            self._compound_samplers = [
+                (sampler[0]._default_kernel_maker(), sampler[1]) for sampler in compound_samplers
+            ]
+            self._compound_set_lengths = compound_set_lengths
+            self._cumulative_lengths = np.cumsum(compound_set_lengths) - compound_set_lengths
+            self._name = name
+            self._parameters = dict(target_log_prob_fn=target_log_prob_fn, name=name)
 
     @property
     def target_log_prob_fn(self):
@@ -68,7 +85,7 @@ class _CompoundStepTF(kernel_base.TransitionKernel):
     def is_calibrated(self):
         return True
 
-    def one_step(self, current_state, previous_kernel_results):
+    def one_step(self, current_state, previous_kernel_results, seed=None):
         """Takes one step of the TransitionKernel
         TODO: More specific fore compound step
         """
@@ -76,6 +93,10 @@ class _CompoundStepTF(kernel_base.TransitionKernel):
             unwrap_state_list = not tf.nest.is_nested(current_state)
             if unwrap_state_list:
                 current_state = [current_state]
+            # TODO: can't use any efficient type of structure here
+            # like tf.TensorArray..., `next_state` can have multiple tf.dtype and
+            # `next_results` stores namedtuple with the set of tf.Tensor's with
+            # multiple tf.dtype too.
             next_state = []
             next_results = []
             previous_kernel_results = previous_kernel_results.compound_results
@@ -87,10 +108,10 @@ class _CompoundStepTF(kernel_base.TransitionKernel):
                 self._cumulative_lengths,
             ):
                 kernel = kernel_create_object(
-                    sampleri, curri, setli, current_state, self._target_log_prob_fn
+                    sampleri, curri, setli, current_state, self._target_log_prob_fn, self.seed
                 )
                 next_state_, next_result_ = kernel.one_step(
-                    current_state[slice(curri, curri + setli)], resulti
+                    current_state[slice(curri, curri + setli)], resulti, seed=seed
                 )
                 # concat state results for flattened list
                 next_state += next_state_

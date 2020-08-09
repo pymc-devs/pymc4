41daff40c96ce8c704cb08331fa3f2dc225216ed
Rasul Karimov <yoatom96@hotmail.com>
2020-08-09 04:27:41 +0300

    fix tests

--- tests/test_compound.py
+++ tests/test_compound.py
@@ -1,5 +1,8 @@
 import pytest
 import pymc4 as pm
+import numpy as np
+import tensorflow as tf
+from pymc4.mcmc.samplers import RandomWalkM
 
 
 @pytest.fixture(scope="function")
@@ -38,6 +41,11 @@ def sampler_type(request):
     return request.param
 
 
+@pytest.fixture(scope="module", params=["randomwalkm", "compound"])
+def discrete_support_sampler_type(request):
+    return request.param
+
+
 def test_samplers_on_compound_model(compound_model, seed, xla_fixture, sampler_type):
     def _execute():
         model = compound_model()
@@ -45,19 +53,37 @@ def test_samplers_on_compound_model(compound_model, seed, xla_fixture, sampler_t
         var1 = round(trace.posterior["compound_model/var1"].mean().item(), 1)
         # int32 dtype variable
         var2 = tf.reduce_sum(trace.posterior["compound_model/var2"]) / (1000 * 10)
-        assert var1 == 0.0 and var2 == 0.5
+        np.testing.assert_allclose(var1, 0.0, atol=0.1)
+        np.testing.assert_allclose(var2, 0.5, atol=0.1)
 
-    if compound_model in ["compound", "randomwalkm"]:
+    if sampler_type in ["compound", "randomwalkm"]:
         # execute normally if sampler supports discrete distributions
         _execute()
     else:
+        # else check for the exception thrown
         with pytest.raises(ValueError):
-            # else check for the exception thrown
             _execute()
+
+def test_compound_model_sampler_method(
+    compound_model, seed, xla_fixture, discrete_support_sampler_type
+):
+    model = compound_model()
+    trace = pm.sample(
+        model,
+        sampler_type=discrete_support_sampler_type,
+        sampler_methods=[("var2", RandomWalkM)],
+        xla_fixture=xla_fixture,
+        seed=seed,
+    )
+    var1 = round(trace.posterior["compound_model/var1"].mean().item(), 1)
+    # int32 dtype variable
+    var2 = tf.reduce_sum(trace.posterior["compound_model/var2"]) / (1000 * 10)
+    np.testing.assert_allclose(var1, 0.0, atol=0.1)
+    np.testing.assert_allclose(var2, 0.5, atol=0.1)
 
 
 def test_samplers_on_simple_model(simple_model, seed, xla_fixture, sampler_type):
     model = simple_model()
     trace = pm.sample(model, sampler_type=sampler_type, xla_fixture=xla_fixture, seed=seed)
     var1 = round(trace.posterior["simple_model/var1"].mean().item(), 1)
-    assert var1 == 0.0
+    np.testing.assert_allclose(var1, 0.0, atol=0.1)

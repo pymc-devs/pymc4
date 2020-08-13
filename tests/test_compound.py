26f2aa835f7ecfc860657914a8250366343321b1
Rasul Karimov <yoatom96@hotmail.com>
2020-08-13 19:15:14 +0300

    restore compound tests

--- tests/test_compound.py
+++ tests/test_compound.py
@@ -74,6 +74,70 @@ def expanded_sampler_type(request):
     return request.param
 
 
+def test_samplers_on_compound_model(compound_model, seed, xla_fixture, sampler_type):
+    def _execute():
+        model = compound_model()
+        trace = pm.sample(model, sampler_type=sampler_type, xla_fixture=xla_fixture, seed=seed)
+        var1 = round(trace.posterior["compound_model/var1"].mean().item(), 1)
+        # int32 dtype variable
+        var2 = tf.reduce_sum(trace.posterior["compound_model/var2"]) / (1000 * 10)
+        np.testing.assert_allclose(var1, 0.0, atol=0.1)
+        np.testing.assert_allclose(var2, 0.5, atol=0.1)
+
+    if sampler_type in ["compound", "randomwalkm"]:
+        # execute normally if sampler supports discrete distributions
+        _execute()
+    else:
+        # else check for the exception thrown
+        with pytest.raises(ValueError):
+            _execute()
+
+
+def test_compound_model_sampler_method(
+    compound_model, seed, xla_fixture, discrete_support_sampler_type
+):
+    model = compound_model()
+    trace = pm.sample(
+        model,
+        sampler_type=discrete_support_sampler_type,
+        sampler_methods=[("var2", RandomWalkM)],
+        xla_fixture=xla_fixture,
+        seed=seed,
+    )
+    var1 = round(trace.posterior["compound_model/var1"].mean().item(), 1)
+    # int32 dtype variable
+    var2 = tf.reduce_sum(trace.posterior["compound_model/var2"]) / (1000 * 10)
+    np.testing.assert_allclose(var1, 0.0, atol=0.1)
+    np.testing.assert_allclose(var2, 0.5, atol=0.1)
+
+
+def test_samplers_on_simple_model(simple_model, seed, xla_fixture, sampler_type):
+    model = simple_model()
+    trace = pm.sample(model, sampler_type=sampler_type, xla_fixture=xla_fixture, seed=seed)
+    var1 = round(trace.posterior["simple_model/var1"].mean().item(), 1)
+    np.testing.assert_allclose(var1, 0.0, atol=0.1)
+
+
+def test_extended_samplers_on_simple_model(simple_model, seed, xla_fixture, expanded_sampler_type):
+    model = simple_model()
+    trace = pm.sample(model, sampler_type=expanded_sampler_type, xla_fixture=xla_fixture, seed=seed)
+    var1 = round(trace.posterior["simple_model/var1"].mean().item(), 1)
+    np.testing.assert_allclose(var1, 0.0, atol=0.1)
+
+
+def test_compound_seed(simple_model, seed, xla_fixture):
+    model = simple_model()
+    trace1 = pm.sample(model, xla_fixture=xla_fixture, seed=seed)
+    trace2 = pm.sample(model, xla_fixture=xla_fixture, seed=seed)
+    np.testing.assert_allclose(
+        tf.norm(trace1.posterior["simple_model"] - trace2.posterior["simple_model"]), 0.0, atol=1e-6
+    )
+
+
+def test_logging(compound_model):
+    raise NotImplementedError
+
+
 def test_sampler_merging(categorical_same_shape, categorical_different_shape):
     model_same = categorical_same_shape()
     model_diff = categorical_different_shape()

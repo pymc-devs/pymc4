245a2684affc0b3b9001b0ede9ddad6b6bd282fb
Rasul Karimov <yoatom96@hotmail.com>
2020-08-15 04:48:45 +0300

    fix final test

--- tests/test_compound.py
+++ tests/test_compound.py
@@ -118,7 +118,7 @@ def test_samplers_on_simple_model(simple_model, xla_fixture, sampler_type):
     np.testing.assert_allclose(var1, 0.0, atol=0.1)
 
 
-def test_extended_samplers_on_simple_model(simple_model, la_fixture, expanded_sampler_type):
+def test_extended_samplers_on_simple_model(simple_model, xla_fixture, expanded_sampler_type):
     model = simple_model()
     trace = pm.sample(model, sampler_type=expanded_sampler_type, xla_fixture=xla_fixture)
     var1 = round(trace.posterior["simple_model/var1"].mean().item(), 1)

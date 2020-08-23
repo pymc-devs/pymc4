Fix doccer tool of covariance functions

pymc4/gp/cov.py
@@ -66,28 +66,29 @@ _common_doc = """feature_ndims : int, optional
         entry of the list, the leftmost ``n`` dimensions of ``i'th`` ``feature_ndims`` column
         are considered for evaluation.
     scale_diag : {Number, array_like}, optional
-        Scaling parameter of the lenght_scale parameter of stationary kernels for
-        performing Automatic Relevance Detection (ARD). Ignored if keyword argument ``ARD=False``."""
+        Scaling parameter of the lenght scales of stationary kernels. When not `None`,
+        transforms the kernel to `FeatureTransformed` kernel. Defaults to `None`."""
 
-_note_doc = """ARD (automatic relevance detection) is performed if the parameter ``length_scale``
-    is a vector or a tensor. To disable this behaviour, a keyword argument
-    ``ARD=False`` needs to be passed. Other keyword arguments that can be passed are:
+_note_doc = """ARD (automatic relevance determination) is not implemented. Vector length scales
+    will be treated as a batch of length scales.
+
+    Other keyword arguments that can be passed are:
     validate_args : bool
         A boolean indicating whether or not to validate arguments. Incurs a little
         overhead when set to ``True``. Its default value is ``False``.
-    name:
+    name : str
         You can optionally give a name to the kernels. All the operations will be
         preformed under the name "<name>/<operation_name>:0"."""
 
 
-@_build_docs
+@_build_docs(_common_doc=_common_doc, _note_doc=_note_doc)
 class Covariance:
     r"""
     Base class of all Covariance functions for Gaussian Process.
 
     Parameters
     ----------
-    %(_common_doc)
+    %(_common_doc)s
 
     Other Parameters
     ----------------
@@ -96,7 +97,7 @@ class Covariance:
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -148,14 +149,12 @@ class Covariance:
         self._feature_ndims = feature_ndims
         self._active_dims = active_dims
         self._scale_diag = scale_diag
-        self._ard = kwargs.pop("ARD", False)
         # Initialize Kernel.
         self._kernel = self._init_kernel(feature_ndims=self.feature_ndims, **kwargs)
         # Wrap the kernel in FeatureScaled kernel for ARD.
-        if self._ard:
-            if self._scale_diag is None:
-                self._scale_diag = tf.constant(1, dtype=self._kernel.dtype)
-            self._kernel = FeatureScaled(self._kernel, scale_diag=self._scale_diag)
+        if self._scale_diag is None:
+            self._scale_diag = tf.constant(1, dtype=self._kernel.dtype)
+        self._kernel = FeatureScaled(self._kernel, scale_diag=self._scale_diag)
 
     @abstractmethod
     def _init_kernel(self, feature_ndims: int, **kwargs) -> PositiveSemidefiniteKernel:
@@ -224,9 +223,10 @@ class Covariance:
     @property
     def ard(self) -> bool:
         r"""Weather ARD is enabled or not."""
-        return self._ard
+        # ARD is not implemented.
+        return False
 
-    def __call__(self, X1: ArrayLike, X2: ArrayLike, diag=False, **kwargs) -> TfTensor:
+    def __call__(self, X1: ArrayLike, X2: ArrayLike, diag: bool = False, **kwargs) -> TfTensor:
         r"""
         Evaluate the covariance matrix between the points ``X1`` and ``X2``.
 
@@ -314,8 +314,6 @@ class Combination(Covariance):
     def _eval_factor(self, factor, X1, X2, diag=False):
         if isinstance(factor, Covariance):
             return factor(X1, X2, diag=diag)
-        if diag:
-            return tf.linalg.diag_part(factor)
         return factor
 
     def merge_factors(self, X1: ArrayLike, X2: ArrayLike, diag=False) -> List[TfTensor]:
@@ -377,7 +375,7 @@ _ls_amp_doc = """length_scale : array_like
         (default=1)"""
 
 
-@_build_docs
+@_build_docs(_ls_amp_doc=_ls_amp_doc, _common_doc=_common_doc, _note_doc=_note_doc)
 class ExpQuad(Stationary):
     r"""
     Exponentiated Quadratic Stationary Covariance Function.
@@ -396,8 +394,8 @@ class ExpQuad(Stationary):
 
     Parameters
     ----------
-    %(_ls_amp_doc)
-    %(_common_doc)
+    %(_ls_amp_doc)s
+    %(_common_doc)s
 
     Other Parameters
     ----------------
@@ -421,7 +419,7 @@ class ExpQuad(Stationary):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -453,7 +451,7 @@ class ExpQuad(Stationary):
         return self._amplitude
 
 
-@_build_docs
+@_build_docs(_common_doc=_common_doc, _note_doc=_note_doc)
 class Constant(Stationary):
     r"""
     A Constant Stationary Covariance Function.
@@ -475,7 +473,7 @@ class Constant(Stationary):
         The constant coefficient indicating the covariance
         between any two points. It is the constant ``c`` in
         the equation above.
-    %(_common_doc)
+    %(_common_doc)s
 
     Other Parameters
     ----------------
@@ -497,7 +495,7 @@ class Constant(Stationary):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -521,7 +519,7 @@ class Constant(Stationary):
         return self._coef
 
 
-@_build_docs
+@_build_docs(_common_doc=_common_doc, _note_doc=_note_doc)
 class WhiteNoise(Stationary):
     r"""
     White-noise kernel function.
@@ -541,7 +539,7 @@ class WhiteNoise(Stationary):
     ----------
     noise : array_like
         The ``noise_level`` of the kernel.
-    %(_common_doc)
+    %(_common_doc)s
 
     Other Parameters
     ----------------
@@ -566,7 +564,7 @@ class WhiteNoise(Stationary):
     Hence, the ``pymc4.gp.cov.WhiteNoise.evaluate_kernel`` method
     raises a ``NotImplementedError`` when called.
 
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -590,7 +588,7 @@ class WhiteNoise(Stationary):
         return self._noise
 
 
-@_build_docs
+@_build_docs(_ls_amp_doc=_ls_amp_doc, _common_doc=_common_doc, _note_doc=_note_doc)
 class RatQuad(Stationary):
     r"""
     Rational Quadratic Kernel.
@@ -609,12 +607,12 @@ class RatQuad(Stationary):
 
     Parameters
     ----------
-    %(_ls_amp_doc)
+    %(_ls_amp_doc)s
     scale_mixture_rate: array_like, optional
         The mixture of length-scales to use. Equivalent to adding ``ExpQuad``
         kernels with different ``length_scale``s. When this parameter approaches
         infinity, it becomes equivalent to the ``ExpQuad`` kernel. (default=1)
-    %(_common_doc)
+    %(_common_doc)s
 
     Examples
     --------
@@ -629,7 +627,7 @@ class RatQuad(Stationary):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -676,12 +674,14 @@ _matern_doc = r"""Matern family of kernels is a generalization over the RBF fami
     there."""
 
 
-@_build_docs
+@_build_docs(
+    _matern_doc=_matern_doc, _ls_amp_doc=_ls_amp_doc, _common_doc=_common_doc, _note_doc=_note_doc
+)
 class Matern12(Stationary):
     r"""
     Matern 1/2 kernel.
 
-    %(_matern_doc)
+    %(_matern_doc)s
 
     This kernel has the value of :math:`nu = 0.5`.
 
@@ -693,8 +693,8 @@ class Matern12(Stationary):
 
     Parameters
     ----------
-    %(_ls_amp_doc)
-    %(_common_doc)
+    %(_ls_amp_doc)s
+    %(_common_doc)s
 
     Examples
     --------
@@ -709,7 +709,7 @@ class Matern12(Stationary):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -741,12 +741,14 @@ class Matern12(Stationary):
         return self._amplitude
 
 
-@_build_docs
+@_build_docs(
+    _matern_doc=_matern_doc, _ls_amp_doc=_ls_amp_doc, _common_doc=_common_doc, _note_doc=_note_doc
+)
 class Matern32(Stationary):
     r"""
     Matern 3/2 kernel.
 
-    %(_matern_doc)
+    %(_matern_doc)s
 
     This kernel has the value of :math:`nu = 1.5`.
 
@@ -759,8 +761,8 @@ class Matern32(Stationary):
 
     Parameters
     ----------
-    %(_ls_amp_doc)
-    %(_common_doc)
+    %(_ls_amp_doc)s
+    %(_common_doc)s
 
     Examples
     --------
@@ -775,7 +777,7 @@ class Matern32(Stationary):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -807,12 +809,14 @@ class Matern32(Stationary):
         return self._amplitude
 
 
-@_build_docs
+@_build_docs(
+    _matern_doc=_matern_doc, _ls_amp_doc=_ls_amp_doc, _common_doc=_common_doc, _note_doc=_note_doc
+)
 class Matern52(Stationary):
     r"""
     Matern 5/2 kernel.
 
-    %(_matern_doc)
+    %(_matern_doc)s
 
     This kernel has the value of :math:`nu = 2.5`.
 
@@ -826,8 +830,8 @@ class Matern52(Stationary):
 
     Parameters
     ----------
-    %(_ls_amp_doc)
-    %(_common_doc)
+    %(_ls_amp_doc)s
+    %(_common_doc)s
 
     Examples
     --------
@@ -842,7 +846,7 @@ class Matern52(Stationary):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -885,7 +889,7 @@ _linear_doc = """bias_variance : array_like
         This parameter brings all the input values closer to origin by ``shift`` units."""
 
 
-@_build_docs
+@_build_docs(_linear_doc=_linear_doc, _common_doc=_common_doc, _note_doc=_note_doc)
 class Linear(Covariance):
     r"""
     Linear Kernel.
@@ -902,8 +906,8 @@ class Linear(Covariance):
 
     Parameters
     ----------
-    %(_linear_doc)
-    %(_common_doc)
+    %(_linear_doc)s
+    %(_common_doc)s
 
     Examples
     --------
@@ -918,7 +922,7 @@ class Linear(Covariance):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -963,7 +967,7 @@ class Linear(Covariance):
         return self._shift
 
 
-@_build_docs
+@_build_docs(_linear_doc=_linear_doc, _common_doc=_common_doc, _note_doc=_note_doc)
 class Polynomial(Covariance):
     r"""
     Polynomial Kernel.
@@ -985,10 +989,10 @@ class Polynomial(Covariance):
 
     Parameters
     ----------
-    %(_linear_doc)
+    %(_linear_doc)s
     exponent : array_like
         Exponent (degree) of the underlying polynomial function.
-    %(_common_doc)
+    %(_common_doc)s
 
     Examples
     --------
@@ -1003,7 +1007,7 @@ class Polynomial(Covariance):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -1063,7 +1067,9 @@ _period_doc = """period : array_like, optional
         of the respective feature dimension. (default=1)"""
 
 
-@_build_docs
+@_build_docs(
+    _ls_amp_doc=_ls_amp_doc, _period_doc=_period_doc, _common_doc=_common_doc, _note_doc=_note_doc
+)
 class Periodic(Covariance):
     r"""
     Periodic aka Exponential Sine Squared Kernel.
@@ -1081,9 +1087,9 @@ class Periodic(Covariance):
 
     Parameters
     ----------
-    %(_ls_amp_doc)
-    %(_period_doc)
-    %(_common_doc)
+    %(_ls_amp_doc)s
+    %(_period_doc)s
+    %(_common_doc)s
 
     Examples
     --------
@@ -1098,7 +1104,7 @@ class Periodic(Covariance):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -1137,7 +1143,7 @@ class Periodic(Covariance):
         return self._period
 
 
-@_build_docs
+@_build_docs(_ls_amp_doc=_ls_amp_doc, _common_doc=_common_doc, _note_doc=_note_doc)
 class Exponential(Stationary):
     r"""Exponential Kernel.
 
@@ -1146,8 +1152,8 @@ class Exponential(Stationary):
 
     Parameters
     ----------
-    %(_ls_amp_doc)
-    %(_common_doc)
+    %(_ls_amp_doc)s
+    %(_common_doc)s
 
     Examples
     --------
@@ -1162,7 +1168,7 @@ class Exponential(Stationary):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -1194,7 +1200,7 @@ class Exponential(Stationary):
         return self._amplitude
 
 
-@_build_docs
+@_build_docs(_common_doc=_common_doc, _note_doc=_note_doc)
 class Gibbs(Covariance):
     r"""
     Gibbs Non-Stationary kernel.
@@ -1220,7 +1226,7 @@ class Gibbs(Covariance):
     fn_args : tuple, optional
         A tuple of other arguments to be passed to the function. If None,
         defaults to passing no extra arguments.
-    %(_common_doc)
+    %(_common_doc)s
 
     Examples
     --------
@@ -1235,7 +1241,7 @@ class Gibbs(Covariance):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -1264,7 +1270,7 @@ class Gibbs(Covariance):
         )
 
 
-@_build_docs
+@_build_docs(_ls_amp_doc=_ls_amp_doc, _common_doc=_common_doc, _note_doc=_note_doc)
 class Cosine(Covariance):
     r"""
     Cosine kernel.
@@ -1280,8 +1286,8 @@ class Cosine(Covariance):
 
     Parameters
     ----------
-    %(_ls_amp_doc)
-    %(_common_doc)
+    %(_ls_amp_doc)s
+    %(_common_doc)s
 
     Examples
     --------
@@ -1296,7 +1302,7 @@ class Cosine(Covariance):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -1328,7 +1334,7 @@ class Cosine(Covariance):
         return self._amplitude
 
 
-@_build_docs
+@_build_docs(_note_doc=_note_doc)
 class ScaledCov(Covariance):
     r"""
     Scaled Covariance Kernel.
@@ -1360,7 +1366,7 @@ class ScaledCov(Covariance):
 
     Notes
     -----
-    %(_note_doc)
+    %(_note_doc)s
     """
 
     def __init__(
@@ -1416,10 +1422,6 @@ class WarpedInput(Covariance):
         TensorFlow function of ``X`` and additional optional arguments.
     fn_args : tuple, optional
         Additional inputs (besides X or Xs) to warp_func.
-
-    Examples
-    --------
-    TODO
 
     Notes
     -----

tests/test_gp_cov.py
@@ -6,149 +6,151 @@ from pymc4.gp.util import stabilize
 
 import pytest
 
-# Test all the covariance functions in pm.gp module
+# Test all the covariance functions in pm.gp module.
+# Use `float64` datatype as PSD conditions fail with `float32`.
 COV_FUNCS = [
     (
         "ExpQuad",
         {
-            "amplitude": 1.0,
-            "length_scale": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1.0, 0.01831564], [0.01831564, 1.0]], dtype=np.float32),
-            "expected_point": np.array([1.0, 1.0], dtype=np.float32),
+            "amplitude": np.float64(1.0),
+            "length_scale": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1.0, 0.01831564], [0.01831564, 1.0]], dtype=np.float64),
+            "expected_point": np.array([1.0, 1.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "Constant",
         {
-            "coef": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1.0, 1.0], [1.0, 1.0]], dtype=np.float32),
-            "expected_point": np.array([1.0, 1.0], dtype=np.float32),
+            "coef": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1.0, 1.0], [1.0, 1.0]], dtype=np.float64),
+            "expected_point": np.array([1.0, 1.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "WhiteNoise",
         {
-            "noise": 1e-4,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1e-4, 0.0], [0.0, 1e-4]], dtype=np.float32),
+            "noise": np.float64(1e-4),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1e-4, 0.0], [0.0, 1e-4]], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "RatQuad",
         {
-            "amplitude": 1.0,
-            "length_scale": 1.0,
-            "scale_mixture_rate": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1.0, 0.2], [0.2, 1.0]], dtype=np.float32),
-            "expected_point": np.array([1.0, 1.0], dtype=np.float32),
+            "amplitude": np.float64(1.0),
+            "length_scale": np.float64(1.0),
+            "scale_mixture_rate": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1.0, 0.2], [0.2, 1.0]], dtype=np.float64),
+            "expected_point": np.array([1.0, 1.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "Matern12",
         {
-            "amplitude": 1.0,
-            "length_scale": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1.0, 0.05910575], [0.05910575, 1.0]], dtype=np.float32),
-            "expected_point": np.array([1.0, 1.0], dtype=np.float32),
+            "amplitude": np.float64(1.0),
+            "length_scale": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1.0, 0.05910575], [0.05910575, 1.0]], dtype=np.float64),
+            "expected_point": np.array([1.0, 1.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "Matern32",
         {
-            "amplitude": 1.0,
-            "length_scale": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1.0, 0.04397209], [0.04397209, 1.0]], dtype=np.float32),
-            "expected_point": np.array([1.0, 1.0], dtype=np.float32),
+            "amplitude": np.float64(1.0),
+            "length_scale": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1.0, 0.04397209], [0.04397209, 1.0]], dtype=np.float64),
+            "expected_point": np.array([1.0, 1.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "Matern52",
         {
-            "amplitude": 1.0,
-            "length_scale": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1.0, 0.03701404], [0.03701404, 1.0]], dtype=np.float32),
-            "expected_point": np.array([1.0, 1.0], dtype=np.float32),
+            "amplitude": np.float64(1.0),
+            "length_scale": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1.0, 0.03701404], [0.03701404, 1.0]], dtype=np.float64),
+            "expected_point": np.array([1.0, 1.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "Exponential",
         {
-            "amplitude": 1.0,
-            "length_scale": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1.0, 0.24311673], [0.24311673, 1.0]], dtype=np.float32),
-            "expected_point": np.array([1.0, 1.0], dtype=np.float32),
+            "amplitude": np.float64(1.0),
+            "length_scale": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1.0, 0.24311673], [0.24311673, 1.0]], dtype=np.float64),
+            "expected_point": np.array([1.0, 1.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "Gibbs",
         {
-            "length_scale_fn": (lambda x: tf.ones(x.shape)),
-            "test_points": [np.array([[1.0], [3.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1.0, 0.13533528], [0.13533528, 1.0]], dtype=np.float32),
-            "expected_point": np.array([1.0, 1.0], dtype=np.float32),
+            "length_scale_fn": (lambda x: tf.ones(x.shape, dtype=tf.float64)),
+            "test_points": [np.array([[1.0], [3.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1.0, 0.13533528], [0.13533528, 1.0]], dtype=np.float64),
+            "expected_point": np.array([1.0, 1.0], dtype=np.float64),
             "feature_ndims": 1,
+            "dtype": tf.float64,
         },
     ),
     (
         "Linear",
         {
-            "bias_variance": 1.0,
-            "slope_variance": 1.0,
-            "shift": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[2.0, 4.0], [4.0, 14.0]], dtype=np.float32),
-            "expected_point": np.array([2.0, 14.0], dtype=np.float32),
+            "bias_variance": np.float64(1.0),
+            "slope_variance": np.float64(1.0),
+            "shift": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[2.0, 4.0], [4.0, 14.0]], dtype=np.float64),
+            "expected_point": np.array([2.0, 14.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "Polynomial",
         {
-            "bias_variance": 1.0,
-            "slope_variance": 1.0,
-            "shift": 1.0,
-            "exponent": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[2.0, 4.0], [4.0, 14.0]], dtype=np.float32),
-            "expected_point": np.array([2.0, 14.0], dtype=np.float32),
+            "bias_variance": np.float64(1.0),
+            "slope_variance": np.float64(1.0),
+            "shift": np.float64(1.0),
+            "exponent": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[2.0, 4.0], [4.0, 14.0]], dtype=np.float64),
+            "expected_point": np.array([2.0, 14.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "Cosine",
         {
-            "length_scale": 1.0,
-            "amplitude": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1.0, 0.47307032], [0.47307032, 1.0]], dtype=np.float32),
-            "expected_point": np.array([1.0, 1.0], dtype=np.float32),
+            "length_scale": np.float64(1.0),
+            "amplitude": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1.0, 0.47307032], [0.47307032, 1.0]], dtype=np.float64),
+            "expected_point": np.array([1.0, 1.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
     (
         "Periodic",
         {
-            "length_scale": 1.0,
-            "amplitude": 1.0,
-            "period": 1.0,
-            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)] * 2,
-            "expected_matrix": np.array([[1.0, 1.0], [1.0, 1.0]], dtype=np.float32),
-            "expected_point": np.array([1.0, 1.0], dtype=np.float32),
+            "length_scale": np.float64(1.0),
+            "amplitude": np.float64(1.0),
+            "period": np.float64(1.0),
+            "test_points": [np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float64)] * 2,
+            "expected_matrix": np.array([[1.0, 1.0], [1.0, 1.0]], dtype=np.float64),
+            "expected_point": np.array([1.0, 1.0], dtype=np.float64),
             "feature_ndims": 1,
         },
     ),
@@ -174,22 +176,26 @@ def build_class_and_get_test_points(name, kwargs):
     return test_points, expected_matrix, expected_point, feature_ndims, KernelClass
 
 
-@pytest.mark.xfail
 def test_cov_funcs_matrix_shape_psd(tf_seed, get_data, get_unique_cov_func):
     attr_name = get_unique_cov_func
     batch_shape, sample_shape, feature_shape, X = get_data
+    X = tf.cast(X, tf.float64)
     kwargs = dict(COV_FUNCS)[attr_name].copy()
     _, _, _, _, KernelClass = build_class_and_get_test_points(attr_name, kwargs)
     kernel = KernelClass(**kwargs, feature_ndims=len(feature_shape))
     cov = stabilize(kernel(X, X))
     assert cov.shape == batch_shape + sample_shape + sample_shape
-    npt.assert_(np.all(np.linalg.eigvals(cov.numpy()) > 0))
+    try:
+        npt.assert_(np.all(np.linalg.eigvals(cov.numpy()) > 0))
+    except:
+        pytest.xfail("PSD condition failed for '{}'.".format(kernel.__class__.__name__))
     npt.assert_allclose(cov, tf.linalg.matrix_transpose(cov), rtol=1e-05)
 
 
 def test_cov_funcs_point_eval_shape(tf_seed, get_data, get_unique_cov_func):
     attr_name = get_unique_cov_func
     batch_shape, sample_shape, feature_shape, X = get_data
+    X = tf.cast(X, tf.float64)
     kwargs = dict(COV_FUNCS)[attr_name].copy()
     _, _, _, _, KernelClass = build_class_and_get_test_points(attr_name, kwargs)
     kernel = KernelClass(**kwargs, feature_ndims=len(feature_shape))
@@ -267,7 +273,7 @@ def test_cov_non_cov_combination(get_cov_func):
         KernelClass,
     ) = build_class_and_get_test_points(attr_name, kwargs)
     kernel = KernelClass(feature_ndims=feature_ndims, **kwargs)
-    others = [2.0, np.array([2.0])]
+    others = [np.float64(2.0), np.array([2.0], dtype=np.float64)]
     for other in others:
         kernel_radd = other * kernel
         kernel_rmul = other + kernel
@@ -297,26 +303,26 @@ def test_cov_funcs_diag(get_cov_func):
 
 
 def test_cov_funcs_active_dims(tf_seed):
-    X1 = tf.random.normal((20, 10, 5, 5, 3))
-    X2 = tf.random.normal((20, 10, 5, 5, 3))
+    X1 = tf.random.normal((20, 10, 5, 5, 3), dtype=tf.float64)
+    X2 = tf.random.normal((20, 10, 5, 5, 3), dtype=tf.float64)
     feature_ndims = 3
-    refkernel = pm.gp.cov.ExpQuad(1.0, 1.0, 3)
+    refkernel = pm.gp.cov.ExpQuad(np.float64(1.0), np.float64(1.0), 3)
 
     active_dims = [4, 3]
-    kernel = pm.gp.cov.ExpQuad(1.0, 1.0, feature_ndims, active_dims)
+    kernel = pm.gp.cov.ExpQuad(np.float64(1.0), np.float64(1.0), feature_ndims, active_dims)
 
     cov_matrix = kernel(X1, X2)
     expected_matrix = refkernel(X1[:, :, :4, :3, :], X2[:, :, :4, :3, :])
     npt.assert_allclose(cov_matrix, expected_matrix, rtol=1e-05)
 
     active_dims = [4, 3, 1]
-    kernel = pm.gp.cov.ExpQuad(1.0, 1.0, feature_ndims, active_dims)
+    kernel = pm.gp.cov.ExpQuad(np.float64(1.0), np.float64(1.0), feature_ndims, active_dims)
     cov_matrix = kernel(X1, X2)
     expected_matrix = refkernel(X1[:, :, :4, :3, :1], X2[:, :, :4, :3, :1])
     npt.assert_allclose(cov_matrix, expected_matrix, rtol=1e-05)
 
     active_dims = [[1, 3], 2, 2]
-    kernel = pm.gp.cov.ExpQuad(1.0, 1.0, feature_ndims, active_dims)
+    kernel = pm.gp.cov.ExpQuad(np.float64(1.0), np.float64(1.0), feature_ndims, active_dims)
     cov_matrix = kernel(X1, X2)
     X1new = tf.stack([X1[:, :, 1, :2, :2], X1[:, :, 3, :2, :2]], axis=2)
     X2new = tf.stack([X2[:, :, 1, :2, :2], X2[:, :, 3, :2, :2]], axis=2)
@@ -327,7 +333,7 @@ def test_cov_funcs_active_dims(tf_seed):
 def test_cov_funcs_invalid_feature_ndims():
     feature_ndims = 0
     with pytest.raises(ValueError, match=r"expected 'feature_ndims' to be an integer"):
-        kernel = pm.gp.cov.ExpQuad(1.0, 1.0, feature_ndims)
+        kernel = pm.gp.cov.ExpQuad(np.float64(1.0), np.float64(1.0), feature_ndims)
 
 
 def test_cov_funcs_invalid_active_dims():
@@ -336,13 +342,14 @@ def test_cov_funcs_invalid_active_dims():
         ValueError, match=r"active_dims' contain more entries than number of feature dimensions"
     ):
         active_dims = [1, 2, 3, 4, 5]
-        kernel = pm.gp.cov.ExpQuad(1.0, 1.0, feature_ndims, active_dims)
+        kernel = pm.gp.cov.ExpQuad(np.float64(1.0), np.float64(1.0), feature_ndims, active_dims)
 
 
 def test_scaled_cov_kernel_shapes_and_psd(tf_seed, get_data):
     batch_shape, sample_shape, feature_shape, X = get_data
-    k = pm.gp.cov.ExpQuad(1.0, feature_ndims=len(feature_shape))
-    scal_fn = lambda x: tf.ones(x.shape)
+    X = tf.cast(X, dtype=tf.float64)
+    k = pm.gp.cov.ExpQuad(np.float64(1.0), feature_ndims=len(feature_shape))
+    scal_fn = lambda x: tf.ones(x.shape, dtype=tf.float64)
     k_scal = pm.gp.cov.ScaledCov(k, scal_fn)
 
     cov = k_scal(X, X)
@@ -354,19 +361,20 @@ def test_scaled_cov_kernel_shapes_and_psd(tf_seed, get_data):
 
 
 def test_scaled_cov():
-    x = tf.constant([[1.0, 2.0], [3.0, 4.0]])
-    k = pm.gp.cov.ExpQuad(1.0)
-    scal_fn = lambda x: tf.ones(x.shape)
+    x = tf.constant([[1.0, 2.0], [3.0, 4.0]], dtype=tf.float64)
+    k = pm.gp.cov.ExpQuad(np.float64(1.0))
+    scal_fn = lambda x: tf.ones(x.shape, dtype=tf.float64)
     k_scal = pm.gp.cov.ScaledCov(k, scal_fn)
 
     cov = k_scal(x, x)
-    expected = tf.constant([[2.0, 0.03663128], [0.03663128, 2.0]], dtype=np.float32)
+    expected = tf.constant([[2.0, 0.03663128], [0.03663128, 2.0]], dtype=np.float64)
     npt.assert_allclose(cov, expected, rtol=1e-05)
 
 
 def test_warped_cov_kernel_shapes_and_psd(tf_seed, get_data):
     batch_shape, sample_shape, feature_shape, X = get_data
-    k = pm.gp.cov.ExpQuad(1.0, feature_ndims=len(feature_shape))
+    X = tf.cast(X, dtype=tf.float64)
+    k = pm.gp.cov.ExpQuad(np.float64(1.0), feature_ndims=len(feature_shape))
     warp_fn = lambda x: x[..., :1]
     k_warped = pm.gp.cov.WarpedInput(k, warp_fn)
 
@@ -379,8 +387,8 @@ def test_warped_cov_kernel_shapes_and_psd(tf_seed, get_data):
 
 
 def test_warped_cov():
-    x = tf.constant([[1.0, 2.0], [3.0, 4.0]])
-    k = pm.gp.cov.ExpQuad(1.0)
+    x = tf.constant([[1.0, 2.0], [3.0, 4.0]], dtype=tf.float64)
+    k = pm.gp.cov.ExpQuad(np.float64(1.0))
     warp_fn = lambda x: x[:, :1]
     k_warped = pm.gp.cov.WarpedInput(k, warp_fn)
 